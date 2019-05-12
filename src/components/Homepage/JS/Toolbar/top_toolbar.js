f2a7fc4fc08f9413403f4c7932572ec015bdac6b
Daniel Ngozika <danielngozikac@gmail.com>
2019-05-12 15:08:15 +0100

    remove hamburger if user is signed in

--- src/components/Homepage/JS/Toolbar/top_toolbar.js
+++ src/components/Homepage/JS/Toolbar/top_toolbar.js
@@ -1,29 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import Logo from '../../../UI/JS/logo';
-import NavigationItems from './navigation_items';
-import { Hamburger } from '../../../UI/JS/icons'
-import SearchProductInput from '../../../../containers/Homepage/JS/search_product_form';
+import Logo from "../../../UI/JS/logo";
+import NavigationItems from "./navigation_items";
+import { Hamburger } from "../../../UI/JS/icons";
+import SearchProductInput from "../../../../containers/Homepage/JS/search_product_form";
 
-import styles from '../../CSS/Toolbar/top_toolbar.module.css';
+import styles from "../../CSS/Toolbar/top_toolbar.module.css";
 
-const toolbar = (props) => (
+const toolbar = props => (
 	<header className={styles.Toolbar}>
 		<Logo />
 		<SearchProductInput />
 		<nav className={styles.nav}>
-			<NavigationItems>
-				{props.children}
-			</NavigationItems>
+			<NavigationItems>{props.children}</NavigationItems>
 		</nav>
-		<i className={styles.icon}>
-			<Hamburger
-				HamStyle={styles.HamStyle}
-				hamBarStyle={styles.hamBarStyle}
-				onClick={props.hamburgerClick}
-			/>
-		</i>
+		{localStorage.length === 0 ||
+		(Date.now() > localStorage.expiresIn && !localStorage.fbLoggedIn) ? (
+			<i className={styles.icon}>
+				<Hamburger
+					HamStyle={styles.HamStyle}
+					hamBarStyle={styles.hamBarStyle}
+					onClick={props.hamburgerClick}
+				/>
+			</i>
+		) : null}
 	</header>
-)
+);
 
 export default toolbar;
