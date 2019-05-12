148ab29f076faf1225e176770cfa4bfec4a1dd33
Daniel Ngozika <danielngozikac@gmail.com>
2019-05-12 17:07:28 +0100

    change form styling if hamburger is absent

--- src/containers/Homepage/JS/search_product_form.js
+++ src/containers/Homepage/JS/search_product_form.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 //style
-import styles from '../CSS/search_product_form.module.css';
+import styles from "../CSS/search_product_form.module.css";
 
 //component
-import { renderInputField } from '../../../components/UI/JS/forms'
+import { renderInputField } from "../../../components/UI/JS/forms";
 
 class SearchProductsForm extends Component {
+	loggedInStatus =
+		localStorage.length === 0 ||
+		(Date.now() > localStorage.expiresIn && !localStorage.fbLoggedIn);
 	render() {
 		return (
-			<form className={styles.form}>
+			<form
+				className={
+					this.loggedInStatus ? styles.form : styles.form_wider
+				}
+			>
 				<Field
 					name="search"
 					type="text"
@@ -21,22 +28,25 @@ class SearchProductsForm extends Component {
 					placeholder="search products"
 				/>
 			</form>
-		)
+		);
 	}
 }
 
 function mapStateToProps(state) {
 	return {
 		searchInputFocus: state.general.searchInputFocus
-	}
+	};
 }
 
 function mapDispatchToProps(dispatch) {
-	return bindActionCreators({
-
-	}, dispatch)
+	return bindActionCreators({}, dispatch);
 }
 
 export default reduxForm({
-	form: 'search products'
-})(connect(mapStateToProps, mapDispatchToProps)(SearchProductsForm));
+	form: "search products"
+})(
+	connect(
+		mapStateToProps,
+		mapDispatchToProps
+	)(SearchProductsForm)
+);
