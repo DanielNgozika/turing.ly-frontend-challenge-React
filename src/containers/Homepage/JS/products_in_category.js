08aa57ecc650366f82dda38afd0de5104bb059bb
Daniel Ngozika <danielngozikac@gmail.com>
2019-05-16 14:13:41 +0100

    add header, reorganise products, add butt toolbar

--- src/containers/Homepage/JS/products_in_category.js
+++ src/containers/Homepage/JS/products_in_category.js
@@ -5,24 +5,38 @@ import { bindActionCreators } from "redux";
 //styles
 import styles from "../CSS/products_in_category.module.css";
 
+//components
+import ButtNavItem from "../../../components/Homepage/JS/Toolbar/butt_nav_item";
+import { CategoryIcon } from "../../../components/UI/JS/icons";
+import ButtToolbar from "../../../components/Homepage/JS/Toolbar/bottom_toolbar";
+import EachProduct from "../../../components/Homepage/JS/each_product";
+import Spinner from "../../../components/UI/JS/spinner";
+import Backdrop from "../../../components/UI/JS/top_backdrop";
+import CartCounter from "./cart_counter";
+import LeftSidedrawer from "../../../components/UI/JS/left_side_drawer";
+import DeptsSideNav from "./depts_side_nav";
+
 //actions
 import {
 	getProductsInCategory,
 	showProductDetail,
 	showAttrModal,
-	clickBackDrop
+	clickBackDrop,
+	openDeptSidebar
 } from "../../../actions/general/index";
-
-import EachProduct from "../../../components/Homepage/JS/each_product";
-import Spinner from "../../../components/UI/JS/spinner";
-import Backdrop from "../../../components/UI/JS/top_backdrop";
-import CartCounter from "./cart_counter";
 
 class ProductsInCategory extends Component {
 	render() {
-		if (!this.props.allProducts) return <Spinner id={styles.spinnerPos}/>;
+		if (!this.props.allProducts) return <Spinner id={styles.spinnerPos} />;
 		return (
 			<>
+				<header className={styles.header}>
+					<i
+						className="fas fa-arrow-left"
+						onClick={() => window.history.go(-1)}
+					/>
+					<h4>{window.location.pathname.split("/")[1]}</h4>
+				</header>
 				<div className={styles.div}>
 					{this.props.allProducts.map(prod => (
 						<EachProduct
@@ -36,10 +50,36 @@ class ProductsInCategory extends Component {
 						/>
 					))}
 				</div>
+				<>
+					<LeftSidedrawer
+						open={this.props.deptSidebarOpen ? true : false}
+						onClick={this.props.clickBackDrop}
+					>
+						<DeptsSideNav />
+					</LeftSidedrawer>
+				</>
 				<CartCounter />
 				{this.props.backdropVisible ? (
 					<Backdrop onClick={this.props.clickBackDrop} />
 				) : null}
+				<ButtToolbar>
+					<ButtNavItem
+						buttName="department"
+						icon={
+							<i>
+								<CategoryIcon />
+							</i>
+						}
+						onClick={this.props.openDeptSidebar}
+					/>
+					{localStorage.fbLoggedIn === "true" ||
+					Date.now() < localStorage.expiresIn ? (
+						<ButtNavItem
+							icon={<i className="fas fa-user" />}
+							onClick={this.searchIconClick}
+						/>
+					) : null}
+				</ButtToolbar>
 			</>
 		);
 	}
@@ -50,7 +90,8 @@ function mapStateToProps(state) {
 		allProducts: state.general.productsInCategory,
 		productDetailed: state.general.productDetailedId,
 		backdropVisible: state.general.backdropVisible,
-		attrModalOpen: state.general.productIdAttrModalOpen
+		attrModalOpen: state.general.productIdAttrModalOpen,
+		deptSidebarOpen: state.general.deptSidebarOpen
 	};
 }
 
@@ -60,7 +101,8 @@ function mapDispatchToProps(dispatch) {
 			getProductsInCategory,
 			showProductDetail,
 			clickBackDrop,
-			showAttrModal
+			showAttrModal,
+			openDeptSidebar
 		},
 		dispatch
 	);
