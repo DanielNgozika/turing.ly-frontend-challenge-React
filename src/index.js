96894a08788467edff41f9fb6e00c5a0112ab31b
Daniel Ngozika <danielngozikac@gmail.com>
2019-05-17 17:36:33 +0100

    add route for profile page

--- src/index.js
+++ src/index.js
@@ -1,43 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import promiseMiddleware from 'redux-promise';
-import rootReducer from './reducers/index';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import promiseMiddleware from "redux-promise";
+import rootReducer from "./reducers/index";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 
 //components
-import Homepage from './containers/Homepage/JS/homepage';
-import SignUp from './containers/Users/JS/sign_up';
-import SignIn from './containers/Users/JS/sign_in';
-import ProductsInCategory from './containers/Homepage/JS/products_in_category';
-import CartPage from './containers/Cart/JS/cart_page';
-import SearchResultsPage from './containers/Homepage/JS/search_results_page'
+import Homepage from "./containers/Homepage/JS/homepage";
+import SignUp from "./containers/Users/JS/sign_up";
+import SignIn from "./containers/Users/JS/sign_in";
+import ProductsInCategory from "./containers/Homepage/JS/products_in_category";
+import CartPage from "./containers/Cart/JS/cart_page";
+import SearchResultsPage from "./containers/Homepage/JS/search_results_page";
+import UserProfile from "./containers/Users/JS/profile";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const storeWithMiddleware = composeEnhancers(applyMiddleware(promiseMiddleware))(createStore);
+const storeWithMiddleware = composeEnhancers(
+	applyMiddleware(promiseMiddleware)
+)(createStore);
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route path="/:category/products" component={ProductsInCategory}/>
+				<Route
+					path="/:category/products"
+					component={ProductsInCategory}
+				/>
+				<Route path="/profile" component={UserProfile} />
 				<Route path="/search_results" component={SearchResultsPage} />
 				<Route path="/sign_up" component={SignUp} />
 				<Route path="/sign_in" component={SignIn} />
-				<Route path="/cart" component={CartPage}/>
+				<Route path="/cart" component={CartPage} />
 				<Route exact path="/" component={Homepage} />
 			</Switch>
 		</BrowserRouter>
-	)
-}
+	);
+};
 
 ReactDOM.render(
 	<Provider store={storeWithMiddleware(rootReducer)}>
 		<App />
 	</Provider>,
-	document.getElementById('root')
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
